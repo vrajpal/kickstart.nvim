2d0e4e011fb1bf5e24abe3db5e404abdc7f044b8
Vijai Rajpal <vijai.p.rajpal@gmail.com>
2024-06-22 14:19:39 -0400

    update theme

--- init.lua
+++ init.lua
@@ -780,14 +780,14 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'Mofiqul/vscode.nvim',
+    'ronisbr/nano-theme.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       vim.o.background = 'light'
-      vim.cmd.colorscheme 'vscode'
+      vim.cmd.colorscheme 'nano-theme'
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
